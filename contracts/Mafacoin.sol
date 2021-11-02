9f818c32315b4009b1ee3178d5997a6113a72a02
Alexandre Tolstenko <tolstenko@gmail.com>
2021-11-02 12:29:08 -0300

    feat(enumerableset): remove unused contract

--- contracts/Mafacoin.sol
+++ contracts/Mafacoin.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+//import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMap.sol";
 //import "@openzeppelin/contracts-upgradeable/utils/math/SignedSafeMathUpgradeable.sol";
 //import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 
@@ -74,76 +75,6 @@ interface IDEXRouter {
         address to,
         uint256 deadline
     ) external;
-}
-
-library IterableMapping {
-    struct Map {
-        address[] keys;
-        mapping(address => uint256) values;
-        mapping(address => uint256) indexOf;
-        mapping(address => bool) inserted;
-    }
-
-    function get(Map storage map, address key) public view returns (uint256) {
-        return map.values[key];
-    }
-
-    function getIndexOfKey(Map storage map, address key)
-        public
-        view
-        returns (int256)
-    {
-        if (!map.inserted[key]) {
-            return -1;
-        }
-        return int256(map.indexOf[key]);
-    }
-
-    function getKeyAtIndex(Map storage map, uint256 index)
-        public
-        view
-        returns (address)
-    {
-        return map.keys[index];
-    }
-
-    function size(Map storage map) public view returns (uint256) {
-        return map.keys.length;
-    }
-
-    function set(
-        Map storage map,
-        address key,
-        uint256 val
-    ) public {
-        if (map.inserted[key]) {
-            map.values[key] = val;
-        } else {
-            map.inserted[key] = true;
-            map.values[key] = val;
-            map.indexOf[key] = map.keys.length;
-            map.keys.push(key);
-        }
-    }
-
-    function remove(Map storage map, address key) public {
-        if (!map.inserted[key]) {
-            return;
-        }
-
-        delete map.inserted[key];
-        delete map.values[key];
-
-        uint256 index = map.indexOf[key];
-        uint256 lastIndex = map.keys.length - 1;
-        address lastKey = map.keys[lastIndex];
-
-        map.indexOf[lastKey] = index;
-        delete map.indexOf[key];
-
-        map.keys[index] = lastKey;
-        map.keys.pop();
-    }
 }
 
 contract MafaCoin is Initializable, ERC20Upgradeable, OwnableUpgradeable {
