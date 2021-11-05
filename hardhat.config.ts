7b8196eb36b98fcef0be0c0b702f719730d4db4a
Alexandre Tolstenko <tolstenko@gmail.com>
2021-11-04 23:21:26 -0300

    feat: add bnb networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,6 +20,11 @@ dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 dotenv.config();
 
+const mnemonic = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set MNEMONIC in a .env file");
+}
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -39,8 +44,20 @@ const config: HardhatUserConfig = {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+          process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic}
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic}
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
