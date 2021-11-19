b03da63f3b81980adcaaafce83bca8739f9dff6c
Alexandre Tolstenko <tolstenko@gmail.com>
2021-11-19 20:34:33 -0300

    chore: compiler optimizations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -83,19 +83,19 @@ const config: HardhatUserConfig = {
   },
   solidity: {
     version: "0.8.9",
-    // settings: {
-    //   metadata: {
-    //     // Not including the metadata hash
-    //     // https://github.com/paulrberg/solidity-template/issues/31
-    //     bytecodeHash: "none"
-    //   },
-    //   // You should disable the optimizer when debugging
-    //   // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-    //   optimizer: {
-    //     enabled: true,
-    //     runs: 800
-    //   }
-    // }
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/solidity-template/issues/31
+        bytecodeHash: "none"
+      },
+      // You should disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800
+      }
+    }
   }
 };
 
