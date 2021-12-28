f34d54feff41e22733644b4476006601795ff9f0
Ailton Sávio <ailtonS758@gmail.com>
2021-12-28 18:02:15 -0300

    chore(egg): initiate egg smart contract

---
+++ contracts/NFTs/Egg.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// HatchingComplexity - Se pais sao raros ele tem maior dificuldade para chocar - demora mais
+// pais
+// ao nascer faz o mix genetico dos pais e minta um mafagafo usando como random seed o timestamp
+import "./BaseNft.sol";
+
+contract Egg is BaseNft {
+
+}
