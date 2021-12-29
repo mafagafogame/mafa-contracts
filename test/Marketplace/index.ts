0d3822ba401a3d4b8274ed4df189f4e10bcd2442
Ailton Sávio <ailtonS758@gmail.com>
2021-12-29 20:14:53 -0300

    test(marketplace): test new price features

--- test/Marketplace/index.ts
+++ test/Marketplace/index.ts
@@ -3,15 +3,16 @@ import { expect } from "chai";
 import { artifacts, ethers, waffle, upgrades } from "hardhat";
 import type { Artifact } from "hardhat/types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import fetch from "node-fetch";
 
 import {
   BaseERC1155,
   BaseERC1155__factory,
   MafaCoin,
   Marketplace,
-  Marketplace__factory,
   MarketplaceTestV2,
   MarketplaceTestV2__factory,
+  Marketplace__factory,
 } from "../../typechain";
 import { expandTo18Decimals } from "../shared/utilities";
 
@@ -21,7 +22,8 @@ describe("Unit tests", function () {
   let baseNft: BaseERC1155;
   let owner: SignerWithAddress;
   let account1: SignerWithAddress;
-  const TOKEN_URI = "https://ipfs.io/ipfs/QmWaurhfmT8df3tadivGyqErCR5bhu9wSB59GpLm192mLh/metadata.json";
+  const MAFA_BNB = "0xC53C7F4736F4a6DA25e950e25c58011Fe26B4a93";
+  const BNB_BUSD = "0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16";
 
   before(async function () {
     [owner, account1] = await ethers.getSigners();
@@ -37,19 +39,23 @@ describe("Unit tests", function () {
       await mafacoin.setTeamSellFee(0);
       await mafacoin.setLiquidyFee(0);
       await mafacoin.setLotteryFee(0);
-      await mafacoin.transfer(account1.address, expandTo18Decimals(1000));
+      await mafacoin.transfer(account1.address, expandTo18Decimals(100000));
 
       const baseNftFactory: BaseERC1155__factory = await ethers.getContractFactory("BaseERC1155");
-      baseNft = <BaseERC1155>await upgrades.deployProxy(baseNftFactory, [TOKEN_URI], {
+      baseNft = <BaseERC1155>await upgrades.deployProxy(baseNftFactory, [""], {
         initializer: "initialize",
         kind: "uups",
       });
 
       const marketplaceFactory: Marketplace__factory = await ethers.getContractFactory("Marketplace");
-      marketplace = <Marketplace>await upgrades.deployProxy(marketplaceFactory, [mafacoin.address], {
-        initializer: "initialize",
-        kind: "uups",
-      });
+      marketplace = <Marketplace>await upgrades.deployProxy(
+        marketplaceFactory,
+        [mafacoin.address, MAFA_BNB, BNB_BUSD],
+        {
+          initializer: "initialize",
+          kind: "uups",
+        },
+      );
 
       await baseNft.grantRole(ethers.utils.id("MINTER_ROLE"), marketplace.address);
     });
@@ -57,6 +63,19 @@ describe("Unit tests", function () {
     it("should be deployed with correct values", async function () {
       expect(await marketplace.acceptedToken()).to.equal(mafacoin.address);
       expect(await marketplace.owner()).to.equal(owner.address);
+    });
+
+    it("should return the correct MAFABUSD price", async function () {
+      const response = await fetch(
+        "https://api.pancakeswap.info/api/v2/tokens/0xaf44400a99a9693bf3c2e89b02652babacc5cdb9",
+      );
+      const data = await response.json();
+      const mafaPrice = parseFloat(data.data.price);
+
+      expect(parseFloat(ethers.utils.formatEther(await marketplace.getMAFABUSDprice()))).to.be.within(
+        mafaPrice - 0.01,
+        mafaPrice + 0.01,
+      );
     });
 
     describe("non owner", function () {
@@ -112,19 +131,35 @@ describe("Unit tests", function () {
         );
       });
 
+      it("user should not be able to buy an item if allowances are not enough", async function () {
+        await marketplace.setItemPrice(baseNft.address, 0, expandTo18Decimals(100));
+
+        await mafacoin.connect(account1).approve(marketplace.address, expandTo18Decimals(1));
+
+        await expect(marketplace.connect(account1).buyItem(baseNft.address, 0, 1)).to.be.revertedWith(
+          "Check the token allowance",
+        );
+      });
+
       it("user should be able to buy an item", async function () {
         await marketplace.setItemPrice(baseNft.address, 0, expandTo18Decimals(100));
 
-        await mafacoin.connect(account1).approve(marketplace.address, expandTo18Decimals(100));
+        await mafacoin.connect(account1).approve(marketplace.address, ethers.constants.MaxUint256);
 
         expect(await baseNft.totalSupply(0)).to.equal(0);
+        expect(await mafacoin.balanceOf(account1.address)).to.equal(expandTo18Decimals(100000));
         expect(await baseNft.balanceOf(account1.address, 0)).to.equal(0);
 
-        await expect(marketplace.connect(account1).buyItem(baseNft.address, 0, 1))
-          .to.emit(marketplace, "ProductBought")
-          .withArgs(baseNft.address, 0, owner.address, account1.address, expandTo18Decimals(100), 1);
+        await expect(marketplace.connect(account1).buyItem(baseNft.address, 0, 1)).to.emit(
+          marketplace,
+          "ProductBought",
+        );
 
         expect(await baseNft.totalSupply(0)).to.equal(1);
+        expect(parseFloat(ethers.utils.formatEther(await mafacoin.balanceOf(account1.address)))).to.be.within(
+          97000,
+          98000,
+        );
         expect(await baseNft.balanceOf(account1.address, 0)).to.equal(1);
       });
     });
