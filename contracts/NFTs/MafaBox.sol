3f2e9d69ec87a4fffd9dd085de2101d80e683d0c
Ailton Sávio <ailtonS758@gmail.com>
2021-12-29 23:53:59 -0300

    feat(mafabox): set probabilities to mafagafo mint when opening a box

--- contracts/NFTs/MafaBox.sol
+++ contracts/NFTs/MafaBox.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
@@ -12,17 +12,26 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-import "../NFTs/BaseERC1155.sol";
-import "../NFTs/MafagafoNft.sol";
+import "./BaseERC1155.sol";
+import "./MafagafoNft.sol";
 
 contract MafaBox is BaseERC1155 {
     using SafeMath for uint256;
     using Address for address;
 
+    uint256[] private probabilities;
+
     MafagafoNft public mafagafo;
     uint256 private totalOpen;
 
-    function initialize() public initializer {
+    uint256 public numberOfMafagafos;
+
+    function initialize(address mafagafoAddress) public initializer {
+        mafagafo = MafagafoNft(mafagafoAddress);
+        probabilities = [50, 100, 150, 300, 400];
+        numberOfMafagafos = probabilities.length;
+
+        // TODO migrate to cloudflare
         super.initialize("https://ipfs.io/ipfs/QmWaurhfmT8df3tadivGyqErCR5bhu9wSB59GpLm192mLh/metadata/{id}.json");
     }
 
@@ -31,10 +40,20 @@ contract MafaBox is BaseERC1155 {
 
         super._burn(_msgSender(), id, 1);
 
+        uint256 randomNumber = random();
+
         // TODO: mint a random mafagafo to msg sender
+        uint256 maxValue = 0;
+        for (uint256 i = 0; i < probabilities.length; i++) {
+            maxValue = maxValue.add(probabilities[i]);
+            if (randomNumber < maxValue) {
+                mafagafo.mint(_msgSender(), i);
+            }
+        }
     }
 
     function random() internal view returns (uint256 randomNumber) {
-        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, _msgSender(), totalOpen)));
+        return
+            uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, _msgSender(), totalOpen))).mod(1000);
     }
 }
