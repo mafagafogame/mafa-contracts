5abfae33bc1a596d4a524463b2f3958bcffd6e4c
Ailton Sávio <ailtonS758@gmail.com>
2021-12-29 23:53:15 -0300

    style: run prettier

--- contracts/NFTs/Breeder.sol
+++ contracts/NFTs/Breeder.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
