1731fcb6ee7da6d630fa76f58f1f5b0d16e88b31
Ailton Sávio <ailtonS758@gmail.com>
2022-01-03 22:18:25 -0300

    feat(egg): update struct variable types and functions

--- contracts/NFTs/EggNft.sol
+++ contracts/NFTs/EggNft.sol
@@ -7,17 +7,21 @@ import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
 import "./EggBase.sol";
 import "./MafagafoAvatarNft.sol";
+import "./BrooderNft.sol";
 
-contract Egg is EggBase {
+contract EggNft is EggBase {
     using SafeMathUpgradeable for uint256;
     using AddressUpgradeable for address;
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     MafagafoAvatarNft public mafagafoContract;
+    BrooderNft public brooderContract;
 
-    function initialize(address mafagafoAddress) public initializer {
-        require(mafagafoAddress.isContract(), "NFT address must be a contract");
+    function initialize(address mafagafoAddress, address brooderAddress) public initializer {
+        require(mafagafoAddress.isContract(), "Mafagafo NFT address must be a contract");
+        require(brooderAddress.isContract(), "Brooder NFT address must be a contract");
         mafagafoContract = MafagafoAvatarNft(mafagafoAddress);
+        brooderContract = BrooderNft(brooderAddress);
 
         // todo: add the correct urlbase
         super.initialize("Egg", "EGG", "");
@@ -25,15 +29,16 @@ contract Egg is EggBase {
 
     function mint(
         address _to,
-        bytes32 _version, // todo: review this: I guess a uint16 should be enough
+        uint16 _version,
         bytes32 _genes,
-        uint16 _generation,
-        uint32[] memory _parentsIDs // todo: review this: should be uint256 we can potentially have more than 4294967295 mafagafos
+        uint32 _generation,
+        uint256 _parent1Id,
+        uint256 _parent2Id
     ) public virtual onlyRole(MINTER_ROLE) {
         super.mint(_to);
-        // todo: missing code
 
-        _layEgg(_to, _tokenIdTracker.current(), _version, _genes, _generation, _parentsIDs);
+        _layEgg(_to, _tokenIdTracker.current(), _version, _genes, _generation, _parent1Id, _parent2Id);
+
         _tokenIdTracker.increment();
     }
 
@@ -44,9 +49,16 @@ contract Egg is EggBase {
         Egg memory _egg = egg[id];
         require(block.timestamp < _egg.timer, "Egg is not in time to hatch");
 
-        mafagafoContract.mint2(_msgSender(), _egg.version, _egg.genes, _egg.generation, _egg.parentsIDs);
+        mafagafoContract.mint(_msgSender(), _egg.version, _egg.genes, _egg.generation, _egg.parent1Id, _egg.parent2Id);
     }
 
-    // lower the timer to hatch an egg when on a breeder
-    function lowerTimer() public virtual {}
+    function breedEgg(uint256 id, uint256 brooderId) public virtual {
+        require(ownerOf(id) == _msgSender(), "Sender must be the owner of the egg");
+        require(brooderContract.balanceOf(_msgSender(), brooderId) > 0, "You don't own any of this brooder");
+
+        uint64 time = brooderContract.getBrooder(brooderId);
+        egg[id].timer = egg[id].timer - time;
+
+        brooderContract.burn(_msgSender(), brooderId, 1);
+    }
 }
