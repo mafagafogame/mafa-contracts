0d93ff8f1903258b5a578508c85b90798acd6bd3
Ailton Sávio <ailtonS758@gmail.com>
2022-01-03 22:16:19 -0300

    feat(brooder): update brooder mapping

---
+++ contracts/NFTs/BrooderNft.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+
+import "../NFTs/BaseERC1155.sol";
+
+contract BrooderNft is BaseERC1155 {
+    using SafeMathUpgradeable for uint256;
+    using AddressUpgradeable for address;
+
+    struct Brooder {
+        uint64 time;
+    }
+
+    mapping(uint256 => Brooder) public brooder;
+
+    function initialize() public initializer {
+        super.initialize("https://ipfs.io/ipfs/QmWaurhfmT8df3tadivGyqErCR5bhu9wSB59GpLm192mLh/metadata/{id}.json");
+    }
+
+    function createBrooder(uint256 id, uint64 time) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        brooder[id] = Brooder(time);
+
+        emit BrooderCreated(id, time);
+    }
+
+    function getBrooder(uint256 id) external virtual returns (uint64) {
+        return (brooder[id].time);
+    }
+
+    // EVENTS
+    event BrooderCreated(uint256 id, uint64 time);
+}
