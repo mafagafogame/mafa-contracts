4e4251efa189859870df9ba38e80c7bb21480b50
Ailton Sávio <ailtonS758@gmail.com>
2022-06-27 19:12:20 -0300

    chore: burn 12 mafagafos that are not mixed

--- contracts/MafagoldAction/Burner.sol
+++ contracts/MafagoldAction/Burner.sol
@@ -57,6 +57,38 @@ contract Burner is Initializable, UUPSUpgradeable, PausableUpgradeable, Withdraw
         emit MafagoldsBurned(sender, tokenIds.length, totalMinted);
     }
 
+    function burnMafagafos(uint256[] memory tokenIds) external virtual whenNotPaused {
+        require(tokenIds.length > 0, "No mafagafos to burn");
+        require(tokenIds.length % 12 == 0, "Amount of mafagafos must be multiple of 12");
+        require(totalMinted + tokenIds.length / 12 <= MINT_LIMIT, "Mint limit exceeded");
+
+        address sender = _msgSender();
+
+        require(mafagafoContract.isApprovedForAll(sender, address(this)), "Check the approval of your avatars");
+
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(
+                mafagafoContract.ownerOf(tokenIds[i]) == sender,
+                "You have to own this avatar to be able to burn it"
+            );
+
+            (uint16 mafaVersion, , uint32 generation, , , , , uint256 matings, ) = mafagafoContract.getMafagafo(
+                tokenIds[i]
+            );
+            require(mafaVersion == 0, "You can only burn avatars from version 0");
+            require(generation == 0, "You can only burn avatars from generation 0");
+            require(matings == 0, "You can only burn avatars that have not been mated");
+
+            mafagafoContract.burn(tokenIds[i]);
+        }
+
+        uint256 amountToMint = tokenIds.length / 12;
+
+        require(mintEgg(sender, amountToMint), "Failed to mint eggs");
+
+        emit MafagafosBurned(sender, tokenIds.length, totalMinted);
+    }
+
     function mintEgg(address sender, uint256 amount) internal virtual returns (bool) {
         for (uint256 i = 0; i < amount; i++) {
             mafagafoContract.mint(sender, mafagafoContract.mafaVersion(), bytes32(_random()), 2, 0, 0, 0x00000000);
@@ -88,15 +120,16 @@ contract Burner is Initializable, UUPSUpgradeable, PausableUpgradeable, Withdraw
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     event MafagoldsBurned(address indexed burner, uint256 indexed amount, uint256 indexed totalMinted);
+    event MafagafosBurned(address indexed burner, uint256 indexed amount, uint256 indexed totalMinted);
 
     uint256[50] private __gap;
 }
 
 contract BurnerMock is Burner {
     function burnMafagolds(uint256[] memory tokenIds) external override {
-        require(totalMinted + tokenIds.length / 3 <= 100, "Mint limit exceeded");
         require(tokenIds.length > 0, "No mafagolds to burn");
         require(tokenIds.length % 3 == 0, "Amount of mafagolds must be multiple of 3");
+        require(totalMinted + tokenIds.length / 3 <= 100, "Mint limit exceeded");
 
         address sender = _msgSender();
 
@@ -120,5 +153,37 @@ contract BurnerMock is Burner {
         require(mintEgg(sender, amountToMint), "Failed to mint eggs");
 
         emit MafagoldsBurned(sender, tokenIds.length, totalMinted);
+    }
+
+    function burnMafagafos(uint256[] memory tokenIds) external override {
+        require(tokenIds.length > 0, "No mafagafos to burn");
+        require(tokenIds.length % 12 == 0, "Amount of mafagafos must be multiple of 12");
+        require(totalMinted + tokenIds.length / 12 <= 25, "Mint limit exceeded");
+
+        address sender = _msgSender();
+
+        require(mafagafoContract.isApprovedForAll(sender, address(this)), "Check the approval of your avatars");
+
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(
+                mafagafoContract.ownerOf(tokenIds[i]) == sender,
+                "You have to own this avatar to be able to burn it"
+            );
+
+            (uint16 mafaVersion, , uint32 generation, , , , , uint256 matings, ) = mafagafoContract.getMafagafo(
+                tokenIds[i]
+            );
+            require(mafaVersion == 0, "You can only burn avatars from version 0");
+            require(generation == 0, "You can only burn avatars from generation 0");
+            require(matings == 0, "You can only burn avatars that have not been mated");
+
+            mafagafoContract.burn(tokenIds[i]);
+        }
+
+        uint256 amountToMint = tokenIds.length / 12;
+
+        require(mintEgg(sender, amountToMint), "Failed to mint eggs");
+
+        emit MafagafosBurned(sender, tokenIds.length, totalMinted);
     }
 }
