a98728495254a277cd18e1f6b0a546b8bae90e19
Ailton Sávio <ailtonS758@gmail.com>
2022-08-05 21:37:45 -0300

    test(mafagafo): update tests for mafagafo nft collection

--- test/MafagafoCollection/index.ts
+++ test/MafagafoCollection/index.ts
@@ -18,7 +18,7 @@ describe("Unit tests", function () {
       const mafagafoFactory = <Mafagafo__factory>await ethers.getContractFactory("Mafagafo");
       mafagafo = <Mafagafo>await upgrades.deployProxy(
         mafagafoFactory,
-        ["https://store.mafagafo.com/assets/mafagafos/0.png", 8000, 2000, accounts[2].address, 420],
+        ["https://store.mafagafo.com/assets/mafagafos/0.png", accounts[2].address, 420],
         {
           initializer: "initialize",
           kind: "uups",
@@ -31,6 +31,8 @@ describe("Unit tests", function () {
       expect(await mafagafo.symbol()).to.equal("MAFA");
       expect(await mafagafo.totalSupply()).to.equal(420);
       expect(await mafagafo.balanceOf(accounts[2].address)).to.equal(420);
+      expect(await mafagafo.ownerOf(0)).to.equal(accounts[2].address);
+      expect(await mafagafo.ownerOf(419)).to.equal(accounts[2].address);
     });
 
     it("Should show each nft its own tokenURI when baseURI is updated", async function () {
@@ -41,6 +43,14 @@ describe("Unit tests", function () {
 
       expect(await mafagafo.tokenURI(0)).to.equal("https://store.mafagafo.com/assets/mafagafos/0");
       expect(await mafagafo.tokenURI(419)).to.equal("https://store.mafagafo.com/assets/mafagafos/419");
+    });
+
+    it("Owner Should be able to mint a NFT", async function () {
+      expect(await mafagafo.balanceOf(accounts[3].address)).to.equal(0);
+      await mafagafo.safeMint(accounts[3].address, 1);
+      expect(await mafagafo.balanceOf(accounts[3].address)).to.equal(1);
+
+      expect(await mafagafo.ownerOf(420)).to.equal(accounts[3].address);
     });
   });
 });
